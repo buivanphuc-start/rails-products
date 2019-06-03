9a2f824d809f7b90e2dd9ee496edc1267e7013bb
buivanphuc-start <phucbv20.12.96@gmail.com>
2019-06-03 08:36:43 +0000

    Ruby On Rails

--- app/javascript/PayTypeSelector/NoPayType.jsx
+++ app/javascript/PayTypeSelector/NoPayType.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 class NoPayType extends React.Component {
     render() {
-        return (<div></div>);
+        return (
+            <div>
+            
+            </div>
+        );
     }
 }
 
